95285a2a18a65442d6bd2b073a41201cb435a986
Roy Mor <roy.mor173@gmail.com>
2016-04-08 01:24:41 +0300

    fix linting

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,11 @@
  * Created by Roy on 02/04/2016.
  */
 
-var gulp = require('gulp');
-var mocha = require('gulp-mocha');
+// npm modules
+var gulp  = require('gulp'),
+	mocha = require('gulp-mocha');
 
 gulp.task('run-tests', function () {
-    return gulp.src('tests/index.js', {read: false})
-        .pipe(mocha({reporter: 'nyan'}));
+	return gulp.src('tests/index.js', {read: false})
+		.pipe(mocha({reporter: 'spec'}));
 });
--- index.js
+++ index.js
@@ -3,66 +3,72 @@
  */
 
 // core modules
-var path = require('path');
+var util = require('util'),
+	path = require('path');
+
+// npm modules
+var _    = require('underscore');
 
 module.exports = {
-    /**
-     * Reformat fs path to be valid.
-     *
-     * @param  {String} fsPath
-     * @return {String}
-     */
-    reformatPath: function(fsPath) {
-        if (!_.isString(fsPath)) {
-            throw new TypeError('path must be of type string');
-        }
-        var segments = fsPath.substring(fsPath.indexOf(path.sep) + 1).split(path.sep);
-        for(var i = 0; i < segments.length; i++) {
-            fsPath = fsPath.replace(segments[i], makeValidFsObjectName(segments[i]));
-        }
-        return fsPath;
-    },
     /**
      * Reformat fs segment to be valid.
      *
      * @param  {String} segment
      * @return {String}
      */
-    reformatSegment: function(segment) {
-        if (!_.isString(segment)) {
-            throw new TypeError('segment must be of type string');
-        }
-        segment = segment
-            .replace(/[/?<>\\,:*|"]/g, '')
-            .replace(/CON|PRN|AUX|CLOCK\$|NUL|COM[1-9]|LPT[1-9]/ig, 'DEVICE_NAME');
-        var invalidTrailingCharactersRegex = /(?:[.\s]+)$/;
-        while (invalidTrailingCharactersRegex.test(segment)) {
-            segment = segment.replace(invalidTrailingCharactersRegex, '');
-        }
-        return segment;
-    },
+	reformatSegment: function(segment) {
+		if (!_.isString(segment)) {
+			throw new TypeError('segment must be of type string');
+		}
+		segment = segment
+			.replace(/[/?<>\\,:*|"]/g, '')
+			.replace(/CON|PRN|AUX|CLOCK\$|NUL|COM[1-9]|LPT[1-9]/ig, '');
+		var invalidTrailingCharactersRegex = /(?:[.\s]+)$/;
+		while (invalidTrailingCharactersRegex.test(segment)) {
+			segment = segment.replace(invalidTrailingCharactersRegex, '');
+		}
+		return segment;
+	},
     /**
-     * Checks if fs path is valid.
+     * Reformat fs path to be valid.
      *
      * @param  {String} fsPath
      * @return {String}
      */
-    isPathValid: function(fsPath) {
-        if (!_.isString(fsPath)) {
-            throw new TypeError('path must be of type string');
-        }
-        return fsPath === reformatPath(fsPath);
-    },
+	reformatPath: function(fsPath) {
+		if (!_.isString(fsPath)) {
+			throw new TypeError('path must be of type string');
+		}
+		var segments = fsPath.substring(fsPath.indexOf(path.sep) + 1).split(path.sep);
+		for (var i = 0; i < segments.length; i++) {
+			var replaceWith = module.exports.reformatSegment(segments[i]);
+			var find = util.format('%s%s', replaceWith === '' ? '\\' : '', segments[i]);
+			fsPath = fsPath.replace(find, replaceWith);
+		}
+		return fsPath;
+	},
+    /**
+     * Checks if fs path is valid.
+     *
+     * @param  {String} fsPath
+     * @return {Boolean}
+     */
+	isPathValid: function(fsPath) {
+		if (!_.isString(fsPath)) {
+			throw new TypeError('path must be of type string');
+		}
+		return fsPath === module.exports.reformatPath(fsPath);
+	},
     /**
      * Checks if fs segment is valid.
      *
      * @param  {String} segment
-     * @return {String}
+     * @return {Boolean}
      */
-    isSegmentValid: function(segment) {
-        if (!_.isString(segment)) {
-            throw new TypeError('segment must be of type string');
-        }
-        return segment === reformatSegment(segment);
-    }
+	isSegmentValid: function(segment) {
+		if (!_.isString(segment)) {
+			throw new TypeError('segment must be of type string');
+		}
+		return segment === module.exports.reformatSegment(segment);
+	}
 };
