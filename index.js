6744cd940709129582e194652daef2a0f0e623ee
Roy Mor <roy.mor173@gmail.com>
2016-04-16 02:58:18 +0300

    add jsdoc, change reformatSegment() logic, add reformat and validate test for darwing & linux OSs
    - change reformatSegment() logic by making
       illegal characters, trailing characters, and illegal
       names into OS respective regex variables

--- index.js
+++ index.js
@@ -4,71 +4,93 @@
 
 // core modules
 var util = require('util'),
-	path = require('path');
+	path = require('path'),
+	os   = require('os');
 
 // npm modules
 var _    = require('underscore');
 
+var osType = os.type();
+
+var illegalCharacters;
+var illegalTrailingCharacters;
+var illegalNames;
+
+if (osType === "Linux" || osType === "Darwin") {
+	illegalCharacters = /\//g;
+} else {
+	illegalCharacters = /[/?<>\\,:*|"]/g;
+	illegalTrailingCharacters = /(?:[.\s]+)$/;
+	illegalNames = /CON|PRN|AUX|CLOCK\$|NUL|COM[1-9]|LPT[1-9]/ig;
+}
+
 module.exports = {
     /**
-     * Reformat fs segment to be valid.
+     * Reformat fs segment to be valid
      *
      * @param  {String} segment
+     * @param  {Boolean=true} strict
      * @return {String}
      */
-	reformatSegment: function(segment) {
+	reformatSegment: function(segment, strict) {
 		if (!_.isString(segment)) {
 			throw new TypeError('segment must be of type string');
 		}
-		segment = segment
-			.replace(/[/?<>\\,:*|"]/g, '')
-			.replace(/CON|PRN|AUX|CLOCK\$|NUL|COM[1-9]|LPT[1-9]/ig, '');
-		var invalidTrailingCharactersRegex = /(?:[.\s]+)$/;
-		while (invalidTrailingCharactersRegex.test(segment)) {
-			segment = segment.replace(invalidTrailingCharactersRegex, '');
+		if (!_.isBoolean(strict)) {
+			strict = true;
+		}
+		segment = segment.replace(illegalCharacters, '');
+		if (strict && osType === "Windows_NT") {
+			segment = segment.replace(illegalNames, '');
+			while (illegalTrailingCharacters.test(segment)) {
+				segment = segment.replace(illegalTrailingCharacters, '');
+			}
 		}
 		return segment;
 	},
     /**
-     * Reformat fs path to be valid.
+     * Reformat fs path to be valid
      *
      * @param  {String} fsPath
+     * @param  {Boolean=true} strict
      * @return {String}
      */
-	reformatPath: function(fsPath) {
+	reformatPath: function(fsPath, strict) {
 		if (!_.isString(fsPath)) {
 			throw new TypeError('path must be of type string');
 		}
 		var segments = fsPath.substring(fsPath.indexOf(path.sep) + 1).split(path.sep);
 		for (var i = 0; i < segments.length; i++) {
-			var replaceWith = module.exports.reformatSegment(segments[i]);
-			var find = util.format('%s%s', replaceWith === '' ? '\\' : '', segments[i]);
+			var replaceWith = module.exports.reformatSegment(segments[i], strict);
+			var find = util.format('%s%s', replaceWith === '' ? path.sep : '', segments[i]);
 			fsPath = fsPath.replace(find, replaceWith);
 		}
 		return fsPath;
 	},
     /**
-     * Checks if fs path is valid.
+     * Checks if fs path is valid
      *
      * @param  {String} fsPath
+     * @param  {Boolean=true} strict
      * @return {Boolean}
      */
-	isPathValid: function(fsPath) {
+	isPathValid: function(fsPath, strict) {
 		if (!_.isString(fsPath)) {
 			throw new TypeError('path must be of type string');
 		}
-		return fsPath === module.exports.reformatPath(fsPath);
+		return fsPath === module.exports.reformatPath(fsPath, strict);
 	},
     /**
-     * Checks if fs segment is valid.
+     * Checks if fs segment is valid
      *
      * @param  {String} segment
+     * @param  {Boolean=true} strict
      * @return {Boolean}
      */
-	isSegmentValid: function(segment) {
+	isSegmentValid: function(segment, strict) {
 		if (!_.isString(segment)) {
 			throw new TypeError('segment must be of type string');
 		}
-		return segment === module.exports.reformatSegment(segment);
+		return segment === module.exports.reformatSegment(segment, strict);
 	}
 };
--- tests/index.js
+++ tests/index.js
@@ -16,27 +16,41 @@ var reformatPath    = fspvr.reformatPath,
 	isSegmentValid  = fspvr.isSegmentValid;
 
 describe('#reformat path', function() {
-	it('reformat windows path with device names', function() {
-		reformatPath('C:\\A\\CON\\B\\PRN\\AUX\\CLOCK$\\NUL\\COM1\\COM2\\COM3\\COM4\\COM5\\COM6\\COM7\\COM8\\COM9' +
-			'\\LPT1\\LPT2\\LPT3\\LPT4\\LPT5\\LPT6\\LPT7\\LPT8\\LPT9\\C\\file.txt').should.equal('C:\\A\\B\\C\\file.txt');
+	describe('windows', function() {
+		it('path with illegal names', function() {
+			reformatPath('C:\\A\\CON\\B\\PRN\\AUX\\CLOCK$\\NUL\\COM1\\COM2\\COM3\\COM4\\COM5\\COM6\\COM7\\COM8\\COM9' +
+					'\\LPT1\\LPT2\\LPT3\\LPT4\\LPT5\\LPT6\\LPT7\\LPT8\\LPT9\\C\\file.txt').should.equal('C:\\A\\B\\C\\file.txt');
+		});
+		it('path segment with invalid characters', function() {
+			reformatSegment('>M<y?\\ D,irec*to|ry"').should.equal('My Directory');
+		});
+		it('path segment with invalid trailing characters', function() {
+			reformatSegment('file..    ..').should.equal('file');
+		});
 	});
-	it('reformat windows path segment with invalid characters', function() {
-		reformatSegment('>M<y?\\ D,irec*to|ry"').should.equal('My Directory');
-	});
-	it('reformat windows path segment with the invalid leading characters', function() {
-		reformatSegment('file..    ..').should.equal('file');
+	describe('darwin & linux', function() {
+		it('path segment with invalid characters', function() {
+			reformatSegment('foo/bar').should.equal('foobar');
+		});
 	});
 });
 
 describe('#validate path', function() {
-	it('validate windows path with device names', function() {
-		isPathValid('C:\\A\\CON\\B\\PRN\\AUX\\CLOCK$\\NUL\\COM1\\COM2\\COM3\\COM4\\COM5\\COM6\\COM7\\COM8\\COM9' +
-			'\\LPT1\\LPT2\\LPT3\\LPT4\\LPT5\\LPT6\\LPT7\\LPT8\\C\\LPT9\\file.txt').should.equal(false);
+	describe('windows', function() {
+		it('path with device names', function() {
+			isPathValid('C:\\A\\CON\\B\\PRN\\AUX\\CLOCK$\\NUL\\COM1\\COM2\\COM3\\COM4\\COM5\\COM6\\COM7\\COM8\\COM9' +
+					'\\LPT1\\LPT2\\LPT3\\LPT4\\LPT5\\LPT6\\LPT7\\LPT8\\C\\LPT9\\file.txt').should.equal(false);
+		});
+		it('path segment with invalid characters', function() {
+			isSegmentValid('>M<y?\\ D,irec*to|ry"').should.equal(false);
+		});
+		it('path segment with invalid trailing characters', function() {
+			isSegmentValid('file..    ..').should.equal(false);
+		});
 	});
-	it('validate windows path segment with invalid characters', function() {
-		isSegmentValid('>M<y?\\ D,irec*to|ry"').should.equal(false);
-	});
-	it('reformat windows path segment with the invalid leading characters', function() {
-		isSegmentValid('file..    ..').should.equal(false);
+	describe('darwin & linux', function() {
+		it('validate darwin and linux path segment with invalid characters', function() {
+			isSegmentValid('foo/bar').should.equal(false);
+		});
 	});
 });
