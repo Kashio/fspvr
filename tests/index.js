516d2b7004885ceaa741347f6802a3306699e21d
Roy Mor <roy.mor173@gmail.com>
2016-04-16 04:31:14 +0300

    add mocks for os.type() to return linux/windows_nt respectivly to the tests

--- tests/index.js
+++ tests/index.js
@@ -4,8 +4,9 @@
 
 // npm modules
 /*eslint-disable no-unused-vars*/
-var should          = require('chai').should();
+var should          = require('chai').should(),
 /*eslint-enable no-unused-vars*/
+	mockery         = require('mockery');
 
 // lib modules
 var fspvr           = require('../index');
@@ -15,42 +16,71 @@ var reformatPath    = fspvr.reformatPath,
 	isPathValid     = fspvr.isPathValid,
 	isSegmentValid  = fspvr.isSegmentValid;
 
-describe('#reformat path', function() {
-	describe('windows', function() {
-		it('path with illegal names', function() {
-			reformatPath('C:\\A\\CON\\B\\PRN\\AUX\\CLOCK$\\NUL\\COM1\\COM2\\COM3\\COM4\\COM5\\COM6\\COM7\\COM8\\COM9' +
-					'\\LPT1\\LPT2\\LPT3\\LPT4\\LPT5\\LPT6\\LPT7\\LPT8\\LPT9\\C\\file.txt').should.equal('C:\\A\\B\\C\\file.txt');
-		});
-		it('path segment with invalid characters', function() {
-			reformatSegment('>M<y?\\ D,irec*to|ry"').should.equal('My Directory');
-		});
-		it('path segment with invalid trailing characters', function() {
-			reformatSegment('file..    ..').should.equal('file');
-		});
-	});
-	describe('darwin & linux', function() {
-		it('path segment with invalid characters', function() {
-			reformatSegment('foo/bar').should.equal('foobar');
-		});
-	});
-});
+mockery.registerAllowable('../index');
 
-describe('#validate path', function() {
-	describe('windows', function() {
-		it('path with device names', function() {
-			isPathValid('C:\\A\\CON\\B\\PRN\\AUX\\CLOCK$\\NUL\\COM1\\COM2\\COM3\\COM4\\COM5\\COM6\\COM7\\COM8\\COM9' +
-					'\\LPT1\\LPT2\\LPT3\\LPT4\\LPT5\\LPT6\\LPT7\\LPT8\\C\\LPT9\\file.txt').should.equal(false);
-		});
-		it('path segment with invalid characters', function() {
-			isSegmentValid('>M<y?\\ D,irec*to|ry"').should.equal(false);
-		});
-		it('path segment with invalid trailing characters', function() {
-			isSegmentValid('file..    ..').should.equal(false);
+var linuxOsMock = {
+	type: function () {
+		return "Linux";
+	}
+};
+
+var windowsOsMock = {
+	type: function () {
+		return "Windows_NT";
+	}
+};
+describe('#fspvr', function() {
+	before(function() {
+		mockery.enable({
+			warnOnReplace: false
 		});
 	});
-	describe('darwin & linux', function() {
-		it('validate darwin and linux path segment with invalid characters', function() {
-			isSegmentValid('foo/bar').should.equal(false);
+	after(function() {
+		mockery.disable();
+	});
+	describe('#windows', function() {
+		before(function() {
+			mockery.registerMock('os', windowsOsMock);
+		});
+		describe('#reformat path', function() {
+			it('path with illegal names', function() {
+				reformatPath('C:\\A\\CON\\B\\PRN\\AUX\\CLOCK$\\NUL\\COM1\\COM2\\COM3\\COM4\\COM5\\COM6\\COM7\\COM8\\COM9' +
+						'\\LPT1\\LPT2\\LPT3\\LPT4\\LPT5\\LPT6\\LPT7\\LPT8\\LPT9\\C\\file.txt').should.equal('C:\\A\\B\\C\\file.txt');
+			});
+			it('path segment with invalid characters', function() {
+				reformatSegment('>M<y?\\ D,irec*to|ry"').should.equal('My Directory');
+			});
+			it('path segment with invalid trailing characters', function() {
+				reformatSegment('file..    ..').should.equal('file');
+			});
+		});
+		describe('#validate path', function() {
+			it('path with device names', function() {
+				isPathValid('C:\\A\\CON\\B\\PRN\\AUX\\CLOCK$\\NUL\\COM1\\COM2\\COM3\\COM4\\COM5\\COM6\\COM7\\COM8\\COM9' +
+						'\\LPT1\\LPT2\\LPT3\\LPT4\\LPT5\\LPT6\\LPT7\\LPT8\\C\\LPT9\\file.txt').should.equal(false);
+			});
+			it('path segment with invalid characters', function() {
+				isSegmentValid('>M<y?\\ D,irec*to|ry"').should.equal(false);
+			});
+			it('path segment with invalid trailing characters', function() {
+				isSegmentValid('file..    ..').should.equal(false);
+			});
+		});
+	});
+	describe('#linux & darwin', function() {
+		before(function() {
+			mockery.registerMock('os', linuxOsMock);
+			fspvr = require('../index');
+		});
+		describe('#reformat path', function() {
+			it('path segment with invalid characters', function() {
+				reformatSegment('foo/bar').should.equal('foobar');
+			});
+		});
+		describe('#validate path', function() {
+			it('validate darwin and linux path segment with invalid characters', function() {
+				isSegmentValid('foo/bar').should.equal(false);
+			});
 		});
 	});
 });
